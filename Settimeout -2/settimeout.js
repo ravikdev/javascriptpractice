4b2012fb3b4a84ea7aa7476b58b5b955593581e9
ravikdev <ravikumar71996@gmail.com>
2023-07-18 12:26:32 +0530

    Update settimeout.js

---
+++ Settimeout -2/settimeout.js
@@ -0,0 +1,6 @@
+function showAlert() {
+    alert("Hello, this is a delayed alert!");
+  }
+  
+  // Set a delay of 3 seconds (3000 milliseconds) before showing the alert
+  setTimeout(showAlert, 3000);
